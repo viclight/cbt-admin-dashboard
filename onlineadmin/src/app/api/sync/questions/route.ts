f806d0f3ddb28121f0432cf28427328ca92a933f
victorlite11 <79342321+victorlite11@users.noreply.github.com>
2025-07-07 12:36:29 -0700

    Remove unused NextRequest import to fix ESLint error

--- onlineadmin/src/app/api/sync/questions/route.ts
+++ onlineadmin/src/app/api/sync/questions/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import dbConnect from '../../../../lib/dbConnect';
 import Question from '../../../../models/Question';
 
