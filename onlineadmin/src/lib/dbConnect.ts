dc0d4feee65afa6157cbb8f0ec479d382c90c786
victorlite11 <79342321+victorlite11@users.noreply.github.com>
2025-07-07 13:03:25 -0700

    Remove deprecated mongoose options from dbConnect

--- onlineadmin/src/lib/dbConnect.ts
+++ onlineadmin/src/lib/dbConnect.ts
@@ -17,10 +17,7 @@ async function dbConnect() {
     return cached.conn;
   }
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then((mongoose) => mongoose);
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
   }
   cached.conn = await cached.promise;
   return cached.conn;
